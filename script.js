adffb187d3577a0e3451c955b036a06fc5450b7d
Faojul <faojulazim13@gmail.com>
2024-10-26 19:42:15 +0600

    javascript update

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const errorParagraph = document.querySelector("#errorParagraph");
 const emailField = document.querySelector("#emailField");
 const submitBtn = document.querySelector("#submitBtn");
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-const form = document.querySelector("#form");
 const borderDiv = document.querySelector("#borderDiv");
 emailField.addEventListener("input", (e) => {
   e.preventDefault();
@@ -14,17 +10,29 @@ emailField.addEventListener("input", (e) => {
     borderDiv.classList.remove("border-DesaturatedRed");
     borderDiv.classList.add("border-[hsl(0_93%_68%)]");
     errorParagraph.classList.remove("hidden");
+    errorParagraph.textContent = "Please provide a valid email";
+    errorParagraph.classList.remove("text-lime-600");
+    errorParagraph.classList.add("text-[hsl(0_93%_68%)]");
   } else {
     borderDiv.classList.remove("border-[hsl(0_93%_68%)]");
     borderDiv.classList.add("border-DesaturatedRed");
     errorParagraph.classList.add("hidden");
-    form.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector("#emailField").value = "";
-      errorParagraph.classList.remove("text-SoftRed");
-      errorParagraph.classList.remove("hidden");
-      errorParagraph.classList.add("text-lime-600");
-      errorParagraph.textContent = "Submitted!";
-    });
+  }
+});
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const emailFieldValue = document.querySelector("#emailField").value;
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!regex.test(emailFieldValue)) {
+    errorParagraph.classList.add("text-[hsl(0_93%_68%)]");
+    errorParagraph.classList.remove("hidden");
+    errorParagraph.classList.remove("text-lime-600");
+    errorParagraph.textContent = "Please provide a valid email";
+  } else {
+    errorParagraph.classList.remove("text-[hsl(0_93%_68%)]");
+    errorParagraph.classList.remove("hidden");
+    errorParagraph.classList.add("text-lime-600");
+    errorParagraph.textContent = "Submitted!";
+    emailField.value = "";
   }
 });
